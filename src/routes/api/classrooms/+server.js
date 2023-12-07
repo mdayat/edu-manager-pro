7cb5b498c1d2b373bca7c0bcf1545318578bef45
Muhammad Nur Hidayat <muhnurdayat@gmail.com>
2023-12-07 16:22:21 +0700

    create POST and GET all classroom feature

---
+++ src/routes/api/classrooms/+server.js
@@ -0,0 +1,175 @@
+import { v4 as uuidv4 } from "uuid";
+import { verifyAccessToken } from "../../../lib/server/token.js";
+import { validateObjectKeys } from "../../../lib/server/utils.js";
+import {
+  createClassroom,
+  getClassrooms,
+} from "../../../lib/server/db/classroom.js";
+
+export const POST = ({ request }) => {
+  // Reject request when authorization header is empty
+  if (request.headers.get("Authorization") === null) {
+    return new Response(JSON.stringify("Unauthorized"), {
+      status: 401,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  const promise = new Promise((resolve) => {
+    const accessToken = request.headers
+      .get("Authorization")
+      .split("Bearer ")[1];
+
+    verifyAccessToken(accessToken)
+      .then((token) => {
+        request
+          .json()
+          .then((body) => {
+            const bodyKeys = Object.keys(body);
+            const validKeyNames = ["name", "description"];
+
+            const hasInvalidLength = bodyKeys.length !== validKeyNames.length;
+            const hasValidObjectKeys = validateObjectKeys(
+              validKeyNames,
+              bodyKeys,
+            );
+
+            if (hasInvalidLength || !hasValidObjectKeys) {
+              // Reject request when body payload is invalid
+              const message =
+                "Invalid body payload: Please refer to the api docs about the allowed body payload";
+              resolve(
+                new Response(JSON.stringify(message), {
+                  status: 400,
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                }),
+              );
+            }
+
+            const classroom = {
+              id: uuidv4(),
+              teacher_id: token.sub,
+              ...body,
+            };
+
+            createClassroom(classroom)
+              .then(() => {
+                const message = `New classroom with the "id" of ${classroom.id}, successfully created`;
+                resolve(
+                  new Response(JSON.stringify(message), {
+                    status: 201,
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                  }),
+                );
+              })
+              .catch((err) => {
+                // Reject request when failed to create new student
+                resolve(
+                  new Response(JSON.stringify(err.message), {
+                    status: 500,
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                  }),
+                );
+              });
+          })
+          .catch((err) => {
+            // Reject request when body payload is missing in request body.
+            resolve(
+              new Response(JSON.stringify(err.message), {
+                status: 400,
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }),
+            );
+          });
+      })
+      .catch((err) => {
+        // Reject request when access token is invalid
+        resolve(
+          new Response(JSON.stringify(err.message), {
+            status: 401,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }),
+        );
+      });
+  });
+
+  return promise;
+};
+
+export const GET = ({ request }) => {
+  // Reject request when authorization header is empty
+  if (request.headers.get("Authorization") === null) {
+    return new Response(JSON.stringify("Unauthorized"), {
+      status: 401,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  const promise = new Promise((resolve) => {
+    const accessToken = request.headers
+      .get("Authorization")
+      .split("Bearer ")[1];
+
+    verifyAccessToken(accessToken)
+      .then((token) => {
+        getClassrooms(token.sub)
+          .then((classrooms) => {
+            resolve(
+              new Response(JSON.stringify(classrooms), {
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }),
+            );
+          })
+          .catch((err) => {
+            // Reject request when failed to get all student
+            resolve(
+              new Response(JSON.stringify(err.message), {
+                status: 500,
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              }),
+            );
+          });
+      })
+      .catch((err) => {
+        // Reject request when access token is invalid
+        resolve(
+          new Response(JSON.stringify(err.message), {
+            status: 401,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }),
+        );
+      });
+  });
+
+  return promise;
+};
+
+export const fallback = ({ request }) => {
+  return new Response(JSON.stringify(request.method + " Method Not Allowed"), {
+    status: 405,
+    headers: {
+      "Content-Type": "application/json",
+      Allow: "GET, POST",
+    },
+  });
+};
