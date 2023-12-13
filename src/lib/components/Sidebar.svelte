575a28def00b8f21d6985a128c0bdebf9907df71
Farhan-101st <farhanhandoyo54@gmail.com>
2023-12-13 07:26:23 +0700

    menghapus tag meta, title dan link

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,15 +1,3 @@
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Sidebar Menu </title>
-    <link rel="stylesheet" href="style.css" />
-    <!-- Boxicons CSS -->
-    <link
-      flex
-      href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
-      rel="stylesheet"
-    />
-
 <nav id="navigation drawer" class="sidebar locked">
     <div class="logo_items flex">
       <span class="logo_name">EduManagerPro</span>
